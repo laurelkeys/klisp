1318baefda27a56f19885b0666cf4b44a96cfd0c
Tiago <tiagoloureirochaves@gmail.com>
2019-03-06 11:15:45 -0300

    Write a basic REPL

---
+++ kotlin/src/step0_repl.kt
@@ -0,0 +1,14 @@
+fun main(args : Array<String>) {
+    while (true) {
+        val input = readLine() ?: break // break if null (EOF)
+        print("user> ${rep(input)}")
+    }
+}
+
+fun read(string: String) = string
+
+fun eval(string: String) = string
+
+fun print(string: String) = string
+
+fun rep(string: String) = print(eval(read(string)))
