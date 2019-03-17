f4eb0db07df580ec858c97f7c22386cbcbc6719a
Tiago <tiagoloureirochaves@gmail.com>
2019-03-17 19:25:03 +0000

    Finish step 3

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ A Lisp interpreter written in Kotlin, based on Mal's ([Make-a-Lisp](https://gith
 - [x] [Step 0: The REPL](https://github.com/kanaka/mal/blob/master/process/guide.md#step-0-the-repl)
 - [x] [Step 1: Read and Print](https://github.com/kanaka/mal/blob/master/process/guide.md#step-1-read-and-print)
 - [x] [Step 2: Eval](https://github.com/kanaka/mal/blob/master/process/guide.md#step-2-eval)
-- [ ] [Step 3: Environments](https://github.com/kanaka/mal/blob/master/process/guide.md#step-3-environments)
+- [x] [Step 3: Environments](https://github.com/kanaka/mal/blob/master/process/guide.md#step-3-environments)
 - [ ] [Step 4: If Fn Do](https://github.com/kanaka/mal/blob/master/process/guide.md#step-4-if-fn-do)
 - [ ] [Step 5: Tail call optimization](https://github.com/kanaka/mal/blob/master/process/guide.md#step-5-tail-call-optimization)
 - [ ] [Step 6: Files, Mutation, and Evil](https://github.com/kanaka/mal/blob/master/process/guide.md#step-6-files-mutation-and-evil)
--- kotlin/src/main/Step3.kt
+++ kotlin/src/main/Step3.kt
@@ -1,70 +1,73 @@
 package main
 
-fun main(args: Array<String>) {
+object Step3 {
 
-    val env = Env(outer = null).apply {
-        set(MalSymbol("+"),
-            MalFunction { list: MalList -> list.elements.reduce { a, b -> (a as MalNumber) + (b as MalNumber) } })
-        set(MalSymbol("-"),
-            MalFunction { list: MalList -> list.elements.reduce { a, b -> (a as MalNumber) - (b as MalNumber) } })
-        set(MalSymbol("*"),
-            MalFunction { list: MalList -> list.elements.reduce { a, b -> (a as MalNumber) * (b as MalNumber) } })
-        set(MalSymbol("/"),
-            MalFunction { list: MalList -> list.elements.reduce { a, b -> (a as MalNumber) / (b as MalNumber) } })
-    }
+    fun main(args: Array<String>) {
 
-    while (true) {
-        val input = readLine() ?: break // break if null (EOF)
-        println("user> ${rep(input, env)}")
-    }
-}
-
-fun read(string: String) = Reader.readStr(string)
-
-fun eval(ast: MalType, env: Env): MalType {
-    when {
-        ast !is MalList -> return evalAST(ast, env)
-        ast.elements.isEmpty() -> return ast
-        ast.isSpecialForm("def!") -> {
-            val (key, value) = with(ast.tail()) { head() to tail().head() }
-            return env.set(key as MalSymbol, eval(value, env))
+        val env = Env(outer = null).apply {
+            set(MalSymbol("+"),
+                MalFunction { list: MalList -> list.elements.reduce { a, b -> (a as MalNumber) + (b as MalNumber) } })
+            set(MalSymbol("-"),
+                MalFunction { list: MalList -> list.elements.reduce { a, b -> (a as MalNumber) - (b as MalNumber) } })
+            set(MalSymbol("*"),
+                MalFunction { list: MalList -> list.elements.reduce { a, b -> (a as MalNumber) * (b as MalNumber) } })
+            set(MalSymbol("/"),
+                MalFunction { list: MalList -> list.elements.reduce { a, b -> (a as MalNumber) / (b as MalNumber) } })
         }
-        ast.isSpecialForm("let*") -> {
-            val (bindings, form) = with(ast.tail()) { head() to tail().head() }
-            val childEnv = Env(outer = env)
 
-            if (bindings !is MalList)
-                throw MalException("List expected as the first parameter of let*. Received: $bindings.")
+        while (true) {
+            val input = readLine() ?: break // break if null (EOF)
+            println("user> ${rep(input, env)}")
+        }
+    }
 
-            bindings.elements.iterator().let {
-                while (it.hasNext()) {
-                    val key = it.next()
-                    if (!it.hasNext())
-                        throw MalException("Even number of elements expected on let* call. Received: ${bindings.elements}.")
-                    childEnv.set(key as MalSymbol, eval(it.next(), childEnv))
+    fun read(string: String) = Reader.readStr(string)
+
+    fun eval(ast: MalType, env: Env): MalType {
+        when {
+            ast !is MalList -> return evalAST(ast, env)
+            ast.elements.isEmpty() -> return ast
+            ast.isSpecialForm("def!") -> {
+                val (key, value) = with(ast.tail()) { head() to tail().head() }
+                return env.set(key as MalSymbol, eval(value, env))
+            }
+            ast.isSpecialForm("let*") -> {
+                val (bindings, form) = with(ast.tail()) { head() to tail().head() }
+                val childEnv = Env(outer = env)
+
+                if (bindings !is MalList)
+                    throw MalException("List expected as the first parameter of let*. Received: $bindings.")
+
+                bindings.elements.iterator().let {
+                    while (it.hasNext()) {
+                        val key = it.next()
+                        if (!it.hasNext())
+                            throw MalException("Even number of elements expected on let* call. Received: ${bindings.elements}.")
+                        childEnv.set(key as MalSymbol, eval(it.next(), childEnv))
+                    }
+                }
+
+                return eval(form, childEnv)
+            }
+            else -> {
+                with(evalAST(ast, env)) {
+                    if (this !is MalList) throw MalException("Expected list not found: $this.")
+                    val (func, args) = head() to tail()
+                    if (func !is MalFunction) throw MalException("Expected function not found: $func.")
+                    return func.apply(args)
                 }
             }
-
-            return eval(form, childEnv)
-        }
-        else -> {
-            with(evalAST(ast, env)) {
-                if (this !is MalList) throw MalException("Expected list not found: $this.")
-                val (func, args) = head() to tail()
-                if (func !is MalFunction) throw MalException("Expected function not found: $func.")
-                return func.apply(args)
-            }
         }
     }
+
+    private fun evalAST(ast: MalType, env: Env): MalType =
+        when (ast) {
+            is MalSymbol -> env.get(ast)
+            is MalList -> ast.elements.fold(MalList()) { list, elem -> list.apply { add(eval(elem, env)) } }
+            else -> ast
+        }
+
+    fun print(expr: MalType) = Printer.printStr(expr)
+
+    fun rep(string: String, env: Env) = print(eval(read(string), env))
 }
-
-private fun evalAST(ast: MalType, env: Env): MalType =
-    when (ast) {
-        is MalSymbol -> env.get(ast)
-        is MalList -> ast.elements.fold(MalList()) { list, elem -> list.apply { add(eval(elem, env)) } }
-        else -> ast
-    }
-
-fun print(expr: MalType) = Printer.printStr(expr)
-
-fun rep(string: String, env: Env) = print(eval(read(string), env))
